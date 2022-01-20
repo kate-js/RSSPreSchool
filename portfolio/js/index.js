0f4d8f2d9b93a7e5e4d2e4dcab65703895d12516
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-01-20 19:36:17 +0100

    feat: add popup menu for 768px

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,1 +1,18 @@
-console.log();
+const nav_burger = document.querySelector('.nav-burger');
+const nav = document.querySelector('.nav');
+
+function toggleMenu() {
+    nav_burger.classList.toggle('open');
+    nav.classList.toggle('open');
+}
+nav_burger.addEventListener('click', toggleMenu);
+
+//добавляем класс к nav и выходит меню
+//убираем класс у nav когда выбрана ссылка
+
+// const nav = document.querySelector('.nav');
+
+// function toggleMenu() {
+    
+// }
+// nav_burger.addEventListener('click', toggleMenu);

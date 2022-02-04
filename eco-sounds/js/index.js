82e636da3a7f64b30ae6acea59abb35a400de5f7
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-02-04 10:59:48 +0100

    feat: synchronization main button and links

--- eco-sounds/js/index.js
+++ eco-sounds/js/index.js
@@ -1,23 +1,50 @@
-//audio
 const audioLink = document.querySelectorAll('.bird');
 const button = document.querySelector('.button');
 
-function player(event){
-    let sound = document.getElementById(event.target.dataset.item);
-    sound.currentTime = 0;
+const audio = document.querySelector('.main-audio');
+let isPlay = false;
 
-    if (event.target.classList.contains('aktive')) {
-        event.target.classList.remove('aktive');
-        button.classList.remove('pause-button');
-        button.classList.add('play-button');
-        sound.pause();
-    } else {
-        event.target.classList.add('aktive');
-        sound.play();
+function changeButton() {
+
+    if(button.classList.contains('play-button')) {
+        audio.play();
         button.classList.remove('play-button');
         button.classList.add('pause-button');
+    } else {
+        audio.pause();
+        button.classList.remove('pause-button');
+        button.classList.add('play-button');
     }
-    
 }
 
-audioLink.forEach((el) => el.addEventListener('click', player));
+button.addEventListener('click', changeButton);
+
+
+const navigation = document.querySelector('.navigation');
+let mainImage = document.querySelector('.main');
+
+function changeTheme(e) {
+    if (e.target.classList.contains('bird')) {
+        audio.pause();
+        const bird = e.target.dataset.item;
+        mainImage.className = 'main';
+        audio.src = `./css/assets/sound/${bird}.mp3`;
+        audio.play();
+        button.classList.remove('play-button');
+        button.classList.add('pause-button');
+
+        if (e.target.dataset.item === 'solovey') {
+            mainImage.classList.add('solovey');
+        } else if (e.target.dataset.item === 'drozd') {
+            mainImage.classList.add('drozd');
+        } else if (e.target.dataset.item === 'zarynka') {
+            mainImage.classList.add('zarynka');
+        } else if (e.target.dataset.item === 'javoronok') {
+            mainImage.classList.add('javoronok');
+        } else  {
+            mainImage.classList.add('slavka');
+        }
+    }
+}
+
+navigation.addEventListener('click', changeTheme);

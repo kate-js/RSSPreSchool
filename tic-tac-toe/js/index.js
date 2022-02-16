e2eeaae11459a2c85053177696782abe75fa6010
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-02-16 09:55:31 +0100

    feat: add x and 0

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -4,6 +4,7 @@ let move = 0;
 
 area.addEventListener('click', e => {
     if(e.target.className = 'box') {
-        
+        move % 2 === 0 ? e.target.innerHTML = 'x' : e.target.innerHTML = '0';
+        move++;
     }
 });

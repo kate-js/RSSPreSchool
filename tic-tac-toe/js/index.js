ced8478fd180f16fbe6f29f386fdbaf3fd1a2f4a
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-02-16 10:11:57 +0100

    feat: add prepare Result

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -1,4 +1,5 @@
 const area = document.getElementById('area');
+let result = '';
 
 let move = 0;
 
@@ -6,5 +7,41 @@ area.addEventListener('click', e => {
     if(e.target.className = 'box') {
         move % 2 === 0 ? e.target.innerHTML = 'x' : e.target.innerHTML = '0';
         move++;
+        check();
     }
 });
+
+
+const check = () => {
+    const boxes = document.getElementsByClassName('box');
+    const arr = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for(let i = 0; i < arr.length; i++) {
+        if(
+            boxes[arr[i][0]].innerHTML == 'x' &&
+            boxes[arr[i][1]].innerHTML == 'x' &&
+            boxes[arr[i][2]].innerHTML == 'x' 
+        ) {
+            result = 'крестики';
+            prepareResult(result);
+        } else if (
+            boxes[arr[i][0]].innerHTML == '0' &&
+            boxes[arr[i][1]].innerHTML == '0' &&
+            boxes[arr[i][2]].innerHTML == '0'
+        ) {
+            result = 'нолики';
+            prepareResult(result);
+        }
+    }
+}
+const prepareResult = winner => {
+    console.log(winner);
+}
